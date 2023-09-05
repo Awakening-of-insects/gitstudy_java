91f2208c88add5b23e1e4dc295bea09187d12043
zjl <454838009@qq.com>
2023-09-05 22:29:46 +0800

    object

---
+++ object.java
@@ -0,0 +1,24 @@
+public class object{
+
+
+	public static void main(String args[])
+	{
+		person p = new person();
+		p.speak();
+
+	}
+
+
+}
+class person 
+{
+	String name;
+	int age;
+	public void speak()
+	{
+		System.out.println("l am a good man");
+	}
+
+
+
+}
