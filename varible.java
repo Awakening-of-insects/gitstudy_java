8054ed44c2b84a2c9c45198491218eceeaca4d49
zjl <454838009@qq.com>
2023-09-07 22:36:08 +0800

    variable

---
+++ varible.java
@@ -0,0 +1,17 @@
+public class varible{
+
+	public static void main(String args[]){
+	   cal a= new cal();
+	   a.sum(1,2,3);
+
+  }
+}
+class cal{
+
+      public int sum(int...num){
+
+	 System.out.println("count:"+num.length);
+	    return 0;
+     }
+}
+
