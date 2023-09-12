63a93eb3c02b66855e823e093a158b45287e0129
zjl <454838009@qq.com>
2023-09-12 22:53:28 +0800

    this use

---
+++ Thistest.java
@@ -0,0 +1,30 @@
+public class Thistest{
+
+
+
+	public static void main(String args[])
+	{
+
+		T t1 = new T();
+
+	}
+}	
+	
+class T{
+
+		public T(){
+			this("123",2);
+			System.out.println("1");
+
+		}
+		public T(String name ,int a)
+		{
+
+			System.out.println("2");
+
+		}
+
+
+}
+
+
